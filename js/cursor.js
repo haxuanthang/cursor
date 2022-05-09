9e2dab9c1c28e5b495e2aa3753afe68c4161a7d0
WINTHANG <haxuanthang29495@gmail.com>
2022-05-09 23:54:00 +0900

    no timefck

--- js/cursor.js
+++ js/cursor.js
@@ -1,2 +1,18 @@
 import { gsap } from "gsap";
 import { lerp, getMousePos, getSiblings } from "./utils";
+
+// Grab the mouse position and set it  to mouse state
+// Lấy vị trí chuột và đặt nó ở trạng thái chuột
+
+let mouse = { x: 0, y: 0 };
+window.addEventListener("mousemove", (ev) => (mouse = getMousePos(ev)));
+
+export default class Cursor {
+  constructor(el) {
+    // Varibles
+    this.Cursor = el;
+    this.Cursor.style.opacity = 0;
+    this.Item = document.querySelectorAll(".hero-inner-link-item");
+    this.Hero = document.querySelector(".hero-inner");
+  }
+}
--- js/index.js
+++ js/index.js
@@ -1,3 +1,5 @@
+import Cursor from "./cursor";
+
 window.onload = () => {
-  console.log("hey there cutie ;)");
+  const cursor = new Cursor(document.querySelector(".cursor"));
 };
